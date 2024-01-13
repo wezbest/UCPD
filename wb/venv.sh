b23dd6ca613721cb9f17b4af27e6ad85de4b8e60
wezbest <110793713+wezbest@users.noreply.github.com>
2024-01-13 15:59:24 +0000

    .

--- wb/venv.sh
+++ wb/venv.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env zsh
 # This is for setting up python virtual environment for 3.12
-
 # Install python
+echo ""
+echo "Installing python3.12 & python3.12-venv"
+echo "sudo apt-get install python3.12"
+echo "sudo apt-get install python3.12-venv"
 sudo apt-get install python3.12
 sudo apt-get install python3.12-venv
-
-# Create virtual environment
-python3.12 -m venv rape
-
-# Activate virtuenenv
-source rape/bin/activate
-
-echo "To exit type - deactivate"
+echo "DONE DRINK HER PISS"
+echo "DONE DRINK HER PISS"
+echo ""
+echo "II. Setup venv pussy"
+echo "python3.12 -m venv rape"
+echo "DONE -RAPE READY"
+echo "DONE -RAPE READY"
+echo "DONE -RAPE READY"
+echo ""
+echo ""
+echo "To actiavate run"
+echo "-source rape/bin/activate"
+echo "To deactivage - Exist Fish shell then"
+echo "deactivate"
+echo ""
