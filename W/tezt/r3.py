ea60f7024722341dd701c62187d29c6a217aa087
wezbest <110793713+wezbest@users.noreply.github.com>
2024-01-17 20:07:47 +0000

    .

--- W/tezt/r3.py
+++ W/tezt/r3.py
@@ -5,7 +5,7 @@ from rich.text import Text
 
 console = Console()
 # Create a Text object with centered text for the Panel
-text = Text("Centered Text in a Panel", style="bold", justify="right")
+text = Text("Centered Text in a Panel", style="bold", justify="Left")
 # Create a Panel with the Text object
 panel = Panel(text)
 # Print the Panel
