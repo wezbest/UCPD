fcdac3aaff5f8781161240685cc9ed24cb61b332
wezbest <110793713+wezbest@users.noreply.github.com>
2024-01-18 15:07:56 +0000

    .

---
+++ W/exc/filez/ex78.py
@@ -0,0 +1,14 @@
+'''
+78: First GUI
+'''
+
+# Prettification 
+from rich import print as rprint
+from rich.pretty import pprint
+from rich import inspect
+from rich.console import Console
+from rich.markdown import Markdown
+from rich.panel import Panel
+from rich import box
+from rich.prompt import Prompt
+console = Console()
