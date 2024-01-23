5e82116c5e6a989399b47d66f7dc838e48747956
wezbest <110793713+wezbest@users.noreply.github.com>
2024-01-23 15:29:16 +0000

    .

---
+++ mytests/displ/t1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3.12
+
+'''
+Testing the requests package here
+'''
+
+# Beautifiers
+# ---------------------------------------------------------------
+# Beautifers
+from rich import print as rprint
+from rich.pretty import pprint
+from rich import inspect
+from rich.console import Console
+from rich.markdown import Markdown
+from rich.panel import Panel
+from rich import box
+from rich.prompt import Prompt
+console = Console()
+# ---------------------------------------------------------------
+
+rprint(Panel('Requests package testing'))
+
