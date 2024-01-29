5028de847754f2d90831bb4dc656a8d0d156bb8b
wezbest <110793713+wezbest@users.noreply.github.com>
2024-01-29 03:27:13 +0000

    .

---
+++ W/pysct/main.py
@@ -0,0 +1,17 @@
+'''
+Python script to create a simple script
+'''
+
+from pyscript import document
+from rich import print as rprint
+from rich.pretty import pprint
+from rich import inspect
+from rich.console import Console
+from rich.markdown import Markdown
+from rich.panel import Panel
+from rich import box
+from rich.prompt import Prompt
+console = Console()
+
+
+console.print("panty")
