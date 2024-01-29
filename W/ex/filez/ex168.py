267d58eedbe4481a9f7da853b5adf7079ac1b231
wezbest <110793713+wezbest@users.noreply.github.com>
2024-01-29 07:03:14 +0000

    .

--- W/ex/filez/ex168.py
+++ W/ex/filez/ex168.py
@@ -18,10 +18,36 @@ def ex168():
     """
     Excercise 158 - 
     """
-    console.print(Panel("Excercise 168 - Finonacci Numbers", border_style="magenta"))
+    console.print(
+        Panel("Excercise 168 - Finonacci Numbers", border_style="magenta"))
 
-    # write a function to generate fibonacci sequence 
-    fibo = [0, 1]
-    for i in range(2, 10):
-        fibo.append(fibo[i-1] + fibo[i-2])
-        print(fibo[i])
+    # write a function to generate fibonacci sequence without using python functions 
+    
+    # def fib(n):
+    #     a,b = 0,1 
+    #     for i in range(n):
+    #         yield a
+    #         a,b = b,a+b
+    # for i in fib(20):
+    #     rprint(i)
+
+    #doing the same with a list - this given a nice list response
+    def fib2(n):
+        a,b = 0,1
+        result = []
+        for i in range(n):
+            result.append(a)
+            a,b = b,a+b
+        return result
+    rprint(fib2(20))
+
+    # def fib(n):
+    #     if n == 0:
+    #         return 0
+    #     elif n == 1:
+    #         return 1
+    #     else:
+    #         return fib(n-1) + fib(n-2)
+    
+    # # Call function 
+    # rprint(fib(30))
