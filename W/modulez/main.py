d5710d7eba02d5c4ea273e628e2d680c5be3b51f
wezbest <110793713+wezbest@users.noreply.github.com>
2024-01-31 03:54:20 +0000

    .

--- W/modulez/main.py
+++ W/modulez/main.py
@@ -8,17 +8,23 @@ Testing module system here
 
 # ------------------------------------------------------
 import utility as u
-from bussy import lickk as lq
+import bussy.lickk as lq
+import sho.sho as sho
 # -------------------------------------------------------
 
 
 # Importing the utility.py in the same folder
 lq.inspect(u)
 
-lq.rprint(lq.Panel("Simple utility import", box=lq.box.ROUNDED,
+lq.rprint(lq.Panel("Simple utility import - importing utility.py", box=lq.box.ROUNDED,
                    expand=False,  border_style="magenta"))
 
 lq.rprint(f"""
 Multiplication: {u.multi(10, 20)}
 Division: {u.divi(10, 20)}
 """)
+
+lq.rprint(lq.Panel("Importing sho.py from sho", box=lq.box.ROUNDED,
+                   expand=False,  border_style="magenta"))
+
+lq.pprint(sho.buyy("Laptop"))
--- W/modulez/sho/sho.py
+++ W/modulez/sho/sho.py
@@ -1,6 +1,7 @@
 '''
 Sub folder import of files
 '''
+from bussy import lickk as lq
 
 def buyy(item):
     cart = []
