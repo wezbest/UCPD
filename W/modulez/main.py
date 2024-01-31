0f6ec990950dd668afeb8a36ac17a0fe01b141b7
wezbest <110793713+wezbest@users.noreply.github.com>
2024-01-31 02:12:48 +0000

    .

--- W/modulez/main.py
+++ W/modulez/main.py
@@ -7,7 +7,7 @@ Testing module system here
 # Beauty Panty
 
 # ------------------------------------------------------
-import utility
+import utility as u
 from rich import print as rprint  # For rprinting
 from rich.pretty import pprint  # For pretty printing
 from rich import inspect  # For inspect
@@ -16,9 +16,18 @@ from rich.markdown import Markdown  # For markdown
 from rich.panel import Panel  # For Panel()
 from rich import box  # For Panel Boxes
 from rich.prompt import Prompt  # For Prompting
+from rich.text import Text  # Fot Text
 console = Console()  # Standard code to access console
 # -------------------------------------------------------
 
 
 # Importing the utility.py in the same folder
-inspect(utility, all=True)
+inspect(u)
+
+rprint(Panel("Simple utility import", box=box.ROUNDED,
+       expand=False,  border_style="magenta"))
+
+rprint(f"""
+Multiplication: {u.multi(10, 20)}
+Division: {u.divi(10, 20)}
+""")
