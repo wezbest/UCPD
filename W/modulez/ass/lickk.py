e30ec0588c7eafe80fbfd2aebfa38d9437ca2853
wezbest <110793713+wezbest@users.noreply.github.com>
2024-01-31 07:37:19 +0000

    .

---
+++ W/modulez/ass/lickk.py
@@ -0,0 +1,12 @@
+# Beauty pussy and ass smells
+# ------------------------------------------------------
+from rich import print as rprint  # For rprinting
+from rich.pretty import pprint  # For pretty printing
+from rich import inspect  # For inspect
+from rich.console import Console  # For console.print
+from rich.markdown import Markdown  # For markdown
+from rich.panel import Panel  # For Panel()
+from rich import box  # For Panel Boxes
+from rich.prompt import Prompt  # For Prompting
+console = Console()  # Standard code to access console
+# -------------------------------------------------------
--- W/modulez/ass/tr1.py
+++ W/modulez/ass/tr1.py
@@ -4,28 +4,20 @@ Testing the choices function
 '''
 # ------------------------------------------------------
 import random
-from rich import print as rprint  # For rprinting
-from rich.pretty import pprint  # For pretty printing
-from rich import inspect  # For inspect
-from rich.console import Console  # For console.print
-from rich.markdown import Markdown  # For markdown
-from rich.panel import Panel  # For Panel()
-from rich import box  # For Panel Boxes
-from rich.prompt import Prompt  # For Prompting
-console = Console()  # Standard code to access console
-# -------------------------------------------------------
-
+import lickk as lq
 
 
 choices = [1, 2, 3]
 while True:
     choices_shuffle = random.choice(choices)
-    rprint(f"Debug - Shuffled Number {choices_shuffle}")
+    lq.rprint(
+        f"[yellow bold]Debug - Shuffled Number {choices_shuffle}[/yellow bold]")
     user_choice = int(input("Enter Number of panty:"))
+
     if (user_choice == choices_shuffle):
-        console.print(Panel(f"""Mistress will fart in your face - 
+        lq.console.print(lq.Panel(f"""Mistress will fart in your face - 
 You chose {user_choice} 
-and winner is {choices_shuffle}"""))
+and winner is {choices_shuffle}""", expand=False))
         break
     else:
-        console.print(f"[red] FUCKED !!! [/red]You Chose {user_choice}")
+        lq.console.print(f"[red] FUCKED !!! [/red]You Chose {user_choice}")
