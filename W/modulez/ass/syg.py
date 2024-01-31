0c1c540da6d6dc1fe6d311f9b89828eea23e36b5
wezbest <110793713+wezbest@users.noreply.github.com>
2024-01-31 08:23:48 +0000

    .

---
+++ W/modulez/ass/syg.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3.12
+'''
+syg.py - Doing assignmen 
+- Write a sysrgv python file that accepts arguments using random choice
+'''
+
+import lickk as lq
+import random
+import sys
+
+# Sample code
+'''
+first = sys.argv[1]
+second = sys.argv[2]
+lq.rprint(f"First: {first}, Second: {second}")
+'''
+
+choices = [1, 2, 3]
+randmized_choice = random.choice(choices)
+lq.rprint(f"Random Choice: {randmized_choice}")
+user_input = int(sys.argv[1])
+if (user_input == randmized_choice):
+    lq.console.print(lq.Panel(f"""[green]Winner
+                              Your Choice {user_input}
+                              Random Choice {randmized_choice}
+                              [/green]""", expand=False, title="Winner", border_style="green"))
+else:
+    lq.console.print(lq.Panel(f"""[red]Loser
+                              Your Choice {user_input}
+                              Random Choice {randmized_choice}
+                              [red]""", expand=False, title="Loser", border_style="red"
+                              ))
